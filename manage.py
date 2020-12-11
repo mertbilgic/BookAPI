22fa0c2eb906183432eaeb4f8afc28a4c72ce4d3
Mert <bilgic.mert44@hotmail.com>
2020-12-11 12:09:31 +0300

    Database migrations

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from bookapi import app
+from bookapi import app,db
 
 manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
